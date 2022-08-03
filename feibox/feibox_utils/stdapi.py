e8f3722207d55cf68ab9ebe40ff247be2db7195f
devoter-fyc <fycsfls_winter@126.com>
2022-08-03 19:11:10 +0800

    Released Alpha 1.

--- feibox/feibox_utils/stdapi.py
+++ feibox/feibox_utils/stdapi.py
@@ -15,7 +15,7 @@ import argparse
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 # Constants.
 version = (0,8,0)
-versuffix = "dev7.2"
+versuffix = "a1"
 __null__ = None
 indev_name = "Jupiter"
 
